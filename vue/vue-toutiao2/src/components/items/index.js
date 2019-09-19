35f0d3973e4b49d2f717987ab0033fc69603ec64
Exist <oncwnuGMjfx5IncnrkxzcUbiRpow@git.weixin.qq.com>
2019-09-19 09:07:18 +0800

    清空

--- vue/vue-toutiao2/src/components/items/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * @file 所有item的统一出口
- * @author junhao
-*/
-
-export { default as MultiplePic } from './multiple-pic.vue'
-export { default as SinglePic } from './single-pic.vue'
-// export { default as Agriculture } from './agriculture.vue'
